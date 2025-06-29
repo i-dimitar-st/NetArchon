7679fc389688a604eaf0584c3338e5d963e51173
Dimitar Iliev <i.dimitar.st@gmail.com>
2025-06-29 11:48:29 +1000

    add docs

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@ PYTHONPATH=.
 VENV_BINARY=./venv/bin/python
 SCRIPT=main.py
 
+# -u: Unbuffered stdout/stderr (real-time logs).
+# -O: Enable optimized mode:
+#     - Removes all assert statements (they are not executed).
+#     - Sets the built-in constant __debug__ to False (disables debug-only code blocks).
+
+
 sudo PYTHONDONTWRITEBYTECODE=1 \
      PYTHONPATH=${PYTHONPATH} \
      ${VENV_BINARY} -u -O ${SCRIPT}
