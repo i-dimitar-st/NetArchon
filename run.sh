99b376862635865c0cf1617a4f1e6ef65a5f1736
Dimitar Iliev <i.dimitar.st@gmail.com>
2025-08-06 20:46:45 +1000

    modified run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 MAIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHONPATH=$MAIN_DIR
 VENV_BINARY=$MAIN_DIR/venv/bin/python
-MODULE=app.main
+MAIN=app.main
 
-# -B no pyc files
-# -u unbuffered
-# -o optimisation
-# -m runs as script
+# -B => no pyc files
+# -u => unbuffered
+# -o => optimisation
+# -m => runs as script
 
 sudo PYTHONDONTWRITEBYTECODE=1 \
      PYTHONPATH=${PYTHONPATH} \
-     ${VENV_BINARY} -B -u -O -m ${MODULE}
+     ${VENV_BINARY} -B -u -O -m ${MAIN}
